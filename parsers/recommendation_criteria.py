77c09c649cf9db2e896a949446270dfd54c7f1a3
goeo_ <me@goeo.pink>
2018-11-11 20:17:52 +0300

    added slots back to the RecommendationCriteria class, made the parser understand !r nomod (passes mods = -1), removed unused exception, made the parser check if a mods string is parsable before attempting to parse it

--- parsers/recommendation_criteria.py
+++ parsers/recommendation_criteria.py
@@ -1,4 +1,13 @@
+import re
+
+mods_regex = re.compile(r'(?:EZ|HD|HR|DT|HT|NC|FL)+')
+
+
 class RecommendationCriteria:
+    __slots__ = ["user", "mods", "max_creation_date", "max_length", "max_combo", "max_ar", "max_od", "max_cs",
+                 "max_bpm", "min_creation_date", "min_length", "min_combo", "min_ar", "min_od", "min_cs", "min_bpm",
+                 "targets"]
+
     def __init__(self, user, mods=0, max_creation_date=0, max_length=0, max_combo=0, max_ar=0, max_od=0, max_cs=0,
                  max_bpm=0, min_creation_date=0, min_length=0, min_combo=0, min_ar=0, min_od=0, min_cs=0, min_bpm=0):
         self.user = user
@@ -17,6 +26,7 @@ class RecommendationCriteria:
         self.min_od = min_od
         self.min_cs = min_cs
         self.min_bpm = min_bpm
+        self.targets = None
 
 
 mods_list = ['NF', 'EZ', 'TD', 'HD', 'HR', 'SD', 'DT', 'RX', 'HT', 'NC', 'FL', 'AU', 'SO', 'AP', 'PF',
@@ -31,10 +41,6 @@ def modsify_string(mod_string):
     return sum([2 ** mods_list.index(mod_string[i:i + 2]) for i in range(0, len(mod_string), 2)])
 
 
-class NotARealCriteriaException(Exception):
-    pass
-
-
 def parse_criteria(user, message):
     criteria = RecommendationCriteria(user)
     objects = message.split(" ")
@@ -47,8 +53,14 @@ def parse_criteria(user, message):
 
     # if objects[1] is not a flag, parse it as mods.
     if not any(x in objects[1] for x in "<>="):
-        criteria.mods = modsify_string(objects[1])  # TODO: maybe check if it's actually modsifyable?
-        second_object_is_mods = True
+        if objects[1].upper() == "NOMOD":
+            criteria.mods = -1
+            second_object_is_mods = True
+        elif mods_regex.match(objects[1].upper()):
+            criteria.mods = modsify_string(objects[1].upper())
+            if criteria.mods & 512:
+                criteria.mods |= 64
+            second_object_is_mods = True
 
     # try parsing objects[2:] as the rest of the flags. objects[1:] if objects[1] wasn't mods
     try:
@@ -57,23 +69,23 @@ def parse_criteria(user, message):
         return criteria
 
     for object in objects_left:
-        for x in ["<=", ">=", "<", ">", "="]:
+        for x in ("<=", ">=", "<", ">", "="):
             if x not in object:
                 continue
 
             parts = object.split(x)
 
-            if not parts[0] in ["creation_date", "length", "combo", "ar", "od", "cs", "bpm"]:
+            if parts[0] not in ("creation_date", "length", "combo", "ar", "od", "cs", "bpm"):
                 break
 
             if x == "<=":
-                setattr(criteria, "max_%s" % parts[0], parts[1] + 1)
-            elif x == ">=":
-                setattr(criteria, "min_%s" % parts[0], parts[1] - 1)
-            elif x == "<":
                 setattr(criteria, "max_%s" % parts[0], parts[1])
-            elif x == ">":
+            elif x == ">=":
                 setattr(criteria, "min_%s" % parts[0], parts[1])
+            elif x == "<":
+                setattr(criteria, "max_%s" % parts[0], float(parts[1]) - 0.01)
+            elif x == ">":
+                setattr(criteria, "min_%s" % parts[0], float(parts[1]) + 0.01)
             elif x == "=":
                 setattr(criteria, "max_%s" % parts[0], parts[1])
                 setattr(criteria, "min_%s" % parts[0], parts[1])
