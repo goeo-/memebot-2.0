ed01f7454a2b18317ffa937646afeb25c19d784a
goeo_ <me@goeo.pink>
2018-11-11 20:15:46 +0300

    simplified recommend handler, made a class called Recommendation with a factory function .get(criteria)

--- handlers/recommend.py
+++ handlers/recommend.py
@@ -1,29 +1,12 @@
 from parsers.recommendation_criteria import parse_criteria
+from recommendation.recommendation import Recommendation
+from recommendation.maps import CouldNotFindMapException
 
-user_top_plays = {
-    # "username": (cache time, [top plays])
-}
 
 async def handler(user, message):
     criteria = parse_criteria(user, message)
-    recommendation = recommend(criteria)
-    return form_message(recommendation)
-
-
-class CouldNotDownloadBeatmapException(Exception):
-    pass
-
-
-'''
-Map
-.select()
-.join(Recommended, JOIN.LEFT_OUTER, on=(Recommended.beatmap_id == Map.beatmap_id &
-                                        Recommended.mods.bin_and(Map.enabled_mods) &
-                                        Recommended.username == user &
-                                        Recommended.date + 2592000 < time.time()))
-.where(Recommended.id.is_null() &
-       Map.acc_pp <= target_acc_pp &
-       Map.aim_pp <= target_aim_pp &
-       Map.speed_pp <= target_speed_pp &
-       Map.total_pp.between(target_total_pp, target_total_pp_max))
-'''
+    try:
+        recommendation = await Recommendation.get(criteria)
+    except CouldNotFindMapException:
+        return "Could not find a map for you. Feel free to try !reset"
+    return str(recommendation)
