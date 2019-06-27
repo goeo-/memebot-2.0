d6124f6f5be65e2b55f188c3a5f36366a6fd0fd7
goeo_ <me@goeo.pink>
2019-06-27 22:13:21 +0200

    made the osu api singleton make more sense (makes session on first api call now)

--- singletons/osu_api.py
+++ singletons/osu_api.py
@@ -5,14 +5,12 @@ import aiohttp
 
 @singleton
 class OsuAPI:
-    def __init__(self, session):
-        self.session = session
+    def __init__(self):
+        self.session = None
 
     async def call(self, endpoint, params):
+        if not self.session:
+            self.session = aiohttp.ClientSession()
         params["k"] = Config().config["osu"]["api_key"]
         async with self.session.get("%s%s" % (Config().config["osu"]["api_url"], endpoint), params=params) as resp:
             return await resp.json()
-
-
-async def init():
-    return OsuAPI(aiohttp.ClientSession())
