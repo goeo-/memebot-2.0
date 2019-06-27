8472cb607a7a140c102d03a9a5bc453e6545e9bb
goeo_ <me@goeo.pink>
2019-06-28 00:41:53 +0200

    it's called something else in this instance. very smart

--- recommend/maps.py
+++ recommend/maps.py
@@ -42,7 +42,7 @@ async def find_map(criteria):
         # check if in top plays of user
         # if so, add to recommended, continue
         async with Database().user_locks[criteria.user]:
-            await Database().objects.create(Recommended, beatmap_id=result.beatmap_id, enabled_mods=result.enabled_mods,
+            await Database().objects.create(Recommended, beatmap_id=result.beatmap_id, mods=result.enabled_mods,
                                             username=criteria.user)
         for play in user_best:
             if play['beatmap_id'] == result.beatmap_id and play['enabled_mods'] & result.enabled_mods:
