bf71c61006740b5821a70df936ec07052d3c450f
goeo_ <me@goeo.pink>
2019-06-28 02:43:40 +0200

    oof forgot one/3

--- recommend/maps.py
+++ recommend/maps.py
@@ -21,7 +21,7 @@ async def find_map(criteria):
         if criteria.mods == -1:
             clauses.append((Map.enabled_mods == 0))
         else:
-            clauses.append((Map.enabled_mods.bin_and(criteria.mods)))
+            clauses.append((Map.enabled_mods.bin_and(criteria.mods) == criteria.mods))
 
     query = Map.select()\
                .join(Recommended, JOIN.LEFT_OUTER, on=((Recommended.beatmap_id == Map.beatmap_id) &
